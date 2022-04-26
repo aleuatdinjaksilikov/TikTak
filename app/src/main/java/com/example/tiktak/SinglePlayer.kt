c6ecec2a3e3423da2459360548bc8cc4b62457c9
Your Name <you@example.com>
2022-04-26 17:38:31 +0300

    Single player jagi jaratildi

---
+++ app/src/main/java/com/example/tiktak/SinglePlayer.kt
@@ -0,0 +1,11 @@
+package com.example.tiktak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SinglePlayer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_player)
+    }
+}
